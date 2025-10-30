2d3b35d3b9f08fe0ca65b342552311bde92ab256
Musharraf-712 <musharrafsiddiqui712@gmail.com>
2025-10-30 20:28:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function startClock(){
+	const timer = document.getElementById('timer');
+
+	function updateTime(){
+		const now = new Date();
+		const date = now.toLocalDateString();
+		const time = now.toLocalTimeString();
+		timer.textContent = `${date} ${time}`
+	}
+	updateTime()
+	setInterval(updateTime,1000);
+}
+startClock()
